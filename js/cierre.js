283b3811d63c7b1d96faab7343849fcdb47f91bb
antonelia <antonelia.rodriguez@davinci.edu.ar>
2023-12-11 20:31:58 -0300

    Agregue intro a la pantalla de cierre

--- js/cierre.js
+++ js/cierre.js
@@ -1,3 +1,48 @@
+//Intro
+$(window).on('load', function(){
+  $("html").css("overflow", "hidden");
+  $('#loader').fadeOut();
+  setTimeout(function() {
+      $('.linea-1').removeClass("inicio-animacion");
+       $('.linea-1').addClass("empezar-animacion");
+   }, 600);
+
+   setTimeout(function() {
+      $('.linea-1').fadeOut();
+   }, 5000);
+
+   setTimeout(function() {
+      $('.linea-2').removeClass("inicio-animacion");
+      $('.linea-2').addClass("empezar-animacion");
+  }, 5000);
+
+  setTimeout(function() {
+      $('.linea-2').fadeOut();
+   }, 10000);
+
+  setTimeout(function() {
+      $('.linea-3').removeClass("inicio-animacion");
+      $('.linea-3').addClass("empezar-animacion");
+  }, 10000);
+
+  setTimeout(function() {
+    $('.linea-3').fadeOut();
+ }, 15000);
+
+  setTimeout(function() {
+    $('.linea-4').removeClass("inicio-animacion");
+    $('.linea-4').addClass("empezar-animacion");
+  }, 15000);
+
+  //Despues de la intro empiezan a pasar estas cosas para que empiece la seccion
+  setTimeout(function() {
+    $("#intro").fadeOut();
+    $("html").css("overflow", "auto");
+    $("#section1").removeClass("d-none");
+  }, 20000);
+});
+
+
 //quiz
 let promptText = "An abstract 3D memphis like world that looks made with Blender, it's selectedTime, I'm surrounded by what I think are buildings. The predominant color I see is selectedColor. There are a lot of geometric shapes in 3D floating around, mostly selectedShape and very few spheres, cylinders, cubes and square pyramids. The rest of the colors in this city are dark purple (744593), bluish-green (3E3E63), mint green (6DC2C4), light yellow (E9DF67) and magenta pink (E43C5A)";
 
