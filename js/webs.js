5800b8fb097f998975b8505591216c67685179df
Luciano Walter Iasi <iasi@fktech.net>
2023-12-17 02:10:39 -0300

    rehice la seccion web y agregue libreria skroll

--- js/webs.js
+++ js/webs.js
@@ -4,6 +4,8 @@ var webSonrisanas = false;
 var webPuzzle = false;
 
 $(window).on('load', function(){
+$("#intro").hide();
+
     $(".lamparita").hide();
     $(".scroll-animations-example").hide();
 
@@ -46,55 +48,6 @@ $('#web-sonrisanas').click(function () {
     }
 });
 
-class Example {
-    constructor(options) {
-        this.root = options.root;
-
-        this.init();
-
-        setTimeout(this.showImages.bind(this), 1000);
-    }
-
-    init() {
-        this.scroll = new LocomotiveScroll({
-            el: this.root,
-            direction: 'horizontal',
-            smooth: true,
-            lerp: 0.05,
-            tablet: {
-                smooth: true
-            },
-            smartphone: {
-                smooth: true
-            }
-        });
-
-        this.images = this.root.querySelectorAll('.image');
-
-        [].forEach.call(this.images, (image) => {
-            image.addEventListener('click', () => {
-                image.classList.add('-clicked');
-                this.hideImages();
-            });
-        });
-    }
-
-    showImages() {
-        [].forEach.call(this.images, (image) => {
-            image.classList.remove('-clicked');
-            image.classList.add('-active');
-        });
-    }
-
-    hideImages() {
-        [].forEach.call(this.images, (image) => {
-            image.classList.remove('-active');
-        });
-
-        setTimeout(this.showImages.bind(this), 2000);
-    }
-}
-
 $("#close-indar").click(function () {
     $("#modal-indar").modal('toggle');
 });
@@ -107,11 +60,6 @@ $("#close-ventanas").click(function () {
     $("#modal-ventanas").modal('toggle');
 });
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const example = new Example({
-        root: document.querySelector('.scroll-animations-example')
-    });
-});
 
 
 // Mostrar el modal con el puzzle cuando se clickea la imagen
