34044246958496929e43a4a5aa35643d4bede4c8
antonelia <antonelia.rodriguez@davinci.edu.ar>
2023-12-19 02:14:57 -0300

    ajustes generales

--- js/motion.js
+++ js/motion.js
@@ -13,5 +13,31 @@ function startExplosion() {
     setTimeout(() => {
         window.location.href="brandboard.html";
     }, 1000)
-  }
+};
 
+$(window).on('load', function(){
+    setTimeout(function() {
+        $(".col-transition-1").removeClass("top-0");
+        $(".col-transition-1").addClass("col-transition");
+        $(".col-transition-1").addClass("top100");
+        
+    }, 1000);
+    setTimeout(function() {
+        $(".col-transition-2").removeClass("top-0");
+        $(".col-transition-2").addClass("col-transition");
+        $(".col-transition-2").addClass("top100");
+    }, 1200);
+    setTimeout(function() {
+        $(".col-transition-3").removeClass("top-0");
+        $(".col-transition-3").addClass("col-transition");
+        $(".col-transition-3").addClass("top100");
+    }, 1400);
+    setTimeout(function() {
+        $(".col-transition-4").removeClass("top-0");
+        $(".col-transition-4").addClass("col-transition");
+        $(".col-transition-4").addClass("top100");
+    }, 1600);
+    setTimeout(function() {
+        $(".transition").addClass("z-100");
+    }, 2500);
+});
